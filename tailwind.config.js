1474f3fd3df7ba7a3c544f3e79cde6b3f9dc729b
skochar1 <sk1@wellesley.edu>
2025-07-25 16:48:38 -0700

    lol

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./components/**/*.{js,ts,jsx,tsx}",
+      "./app/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+      extend: {
+        // You can add custom colors, fonts, etc. here
+      },
+    },
+    plugins: [],
+  }
+  
